1edfdfe547a54f732c52951a8538e796af2d9d92
jodheeee <soso99700@naver.com>
2022-12-26 13:55:03 +0900

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, defineProps } from "vue";
 
-defineProps<{ msg: string }>()
+interface Props {
+  msg?: string;
+  name: string;
+}
 
-const count = ref(0)
+const props = defineProps<Props>();
+
+const localData = ref<string>("looo");
+
+const localFun = (parameter: string) => {
+  localData.value = parameter;
+};
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <h1>{{ props.name }}</h1>
+  <h3>{{ props.msg }}</h3>
+  <p>{{ localData }}</p>
+  <p @click="localFun('oool')">Click!</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
